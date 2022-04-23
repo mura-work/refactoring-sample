46076ca3a3f2c80159f5d1c6786d335d2bfd51e6
murata-naoya <murata.small.baseball@gmail.com>
2022-04-24 00:21:20 +0900

    PerformanceCalculatorの作成

--- src/services/session1/createStatementData.js
+++ src/services/session1/createStatementData.js
@@ -1,48 +1,10 @@
 export function createStatementData(invoice, plays) {
   const enrichPerformance = (perf) => {
+    const calculator = new PerformanceCalculator(perf, playFor(perf))
     const result = Object.assign({}, perf)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
-    return result
-  }
-
-  /**
-   * type別の金額計算
-   * @param {*} perf
-   * @return {*}
-   */
-  const amountFor = (perf) => {
-    let result = 0
-    switch (perf.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
-        }
-        result += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${perf.play.name}`);
-      }
-    return result
-  }
-
-  /**
-   * ボリューム特典ポイントの集計
-   * @param {*} perf
-   * @return {*}
-   */
-  const volumeCreditsFor = (perf) => {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
@@ -77,6 +39,52 @@ export function createStatementData(invoice, plays) {
    */
   const playFor = (perf) => {
     return plays[perf.playID]
+  }
+
+  class PerformanceCalculator {
+    constructor(perf, play) {
+      this.performance = perf
+      this.play = play
+    }
+
+    /**
+     * type別の金額計算
+     * @param {*} perf
+     * @return {*}
+     */
+    get amount() {
+      let result = 0
+      switch (this.play.type) {
+        case "tragedy":
+          result = 40000;
+          if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+          }
+          result += 300 * this.performance.audience;
+          break;
+        default:
+          throw new Error(`unknown type: ${this.play.name}`);
+        }
+      return result
+    }
+
+    /**
+     * ボリューム特典ポイントの集計
+     * @param {*} perf
+     * @return {*}
+     */
+    get volumeCreditsFor() {
+      let result = 0;
+      result += Math.max(this.performance.audience - 30, 0);
+      if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+      return result
+    }
   }
 
   const result = {}
